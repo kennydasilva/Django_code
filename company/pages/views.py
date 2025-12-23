2c1f6308912196fdbff6c7d02f4ea16cc5baaf50
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-23 23:18:36 +0200

    addiction of inventoy_list

--- company/pages/views.py
+++ company/pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def home_page_view(request):
+    context={
+        "inventory_list": ["Widget 1","Widget 2","Widget 3"],
+        "greeting": "THAnK you FOR visitING.",
+    }
+    return render(request,"home.html", context)
